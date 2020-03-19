44622c78f0301a18ab27a26bf97c5fc0cf9be125
Connor <cjkneebone@gmail.com>
2020-03-19 21:56:05 +1100

    Conceptual new motor class layouts

--- src/motors/sabertooth.py
+++ src/motors/sabertooth.py
@@ -1,9 +1,25 @@
 # Sabertooth motor connection using serial
-from motor_wrapper import MotorWrapper
+from motor_wrapper import MotorWrapper, ConnectionWrapper
 import serial
 import logging
 
-class SabertoothConnection(MotorWrapper):
+class SabertoothMotor(MotorWrapper):
+    def __init__(self, connection, config):
+        self.connection = connection
+        self.guid = config.get("guid")
+        self.name = config.get("name")
+        self.channel = config.get("channel")
+
+    def set_speed(self, speed):
+        if (self.channel == 1):
+            msg = 64 + round(63 / 100 * max(min(left, 100), -100))
+            self.serial.write(bytes([msg]))
+        elif (self.channel == 2):
+            msg = 192 + round(63 / 100 * max(min(left, 100), -100))
+            self.serial.write(bytes([msg]))
+
+
+class SabertoothConnection(ConnectionWrapper):
     # What type of motor this wrapper handles
     type_ = 'sabertooth'
 
@@ -11,20 +27,9 @@ class SabertoothConnection(MotorWrapper):
         MotorWrapper.__init__(self, config)
         self.port = config.get('port')
         self.baudrate = config.get('baudrate')
-        # Try
         self.serial = serial.Serial(port=self.port, baudrate=self.baudrate)
 
-    def move_raw(self, left=None, right=None):
-        # Left side
-        if left is not None:
-            msg = 64 + round(63 / 100 * max(min(left, 100), -100))
-            self.serial.write(bytes([msg]))
-        # Right side
-        if right is not None:
-            msg = 192 + round(63 / 100 * max(min(right, 100), -100))
-            self.serial.write(bytes([msg]))
-
-    def stop(self):
+    def stop_all(self):
         self.serial.write(bytes([0]))
 
     def close(self):
--- src/motors/virtual.py
+++ src/motors/virtual.py
@@ -1,17 +1,23 @@
 # Virtual motor connection
-from motor_wrapper import MotorWrapper
+from motor_wrapper import MotorWrapper, ConnectionWrapper
 
-class VirtualConnection(MotorWrapper):
+class VirtualMotor(MotorWrapper):
+    def __init__(self, connection, config):
+        self.connection = connection
+        self.guid = config.get("guid")
+        self.name = config.get("name")
+
+    def set_speed(self, speed):
+        pass
+
+class VirtualConnection(ConnectionWrapper):
     # What type of motor this wrapper handles
     type_ = 'virtual'
 
     def __init__(self, config):
         MotorWrapper.__init__(self, config)
 
-    def move_raw(self, left=None, right=None):
-        pass
-
-    def stop(self):
+    def stop_all(self):
         pass
 
     def close(self):
